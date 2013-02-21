f2a64fbc28c29554db4bbb7fb6d2c75484bed0cb
lukax <tecinfolucas@gmail.com>
2013-02-21 20:23:32 -0300

    added new viewmodel

---
+++ LOB.UI.Core/ViewModel/Controls/List/ListPersonViewModel.cs
@@ -0,0 +1,30 @@
+﻿#region Usings
+
+using LOB.Dao.Interface;
+using LOB.Domain.Base;
+using LOB.UI.Core.ViewModel.Controls.List.Base;
+
+#endregion
+
+namespace LOB.UI.Core.ViewModel.Controls.List
+{
+    public class ListPersonViewModel : ListBaseEntityViewModel<Person>
+    {
+        public ListPersonViewModel(Person entity, IRepository repository)
+            : base(entity, repository)
+        {
+        }
+
+        public override bool CanUpdate(object arg)
+        {
+            //TODO: Business logic
+            return true;
+        }
+
+        public override bool CanDelete(object arg)
+        {
+            //TODO: Business logic
+            return true;
+        }
+    }
+}
