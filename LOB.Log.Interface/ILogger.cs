b3f63fe8b4ab34c01b0c5e49cc90fc345664d14d
Lukax <lucas.espindola@outlook.com>
2013-03-12 19:40:25 -0300

    interface for LOB.Log

---
+++ LOB.Log.Interface/ILogger.cs
@@ -0,0 +1,13 @@
+﻿#region Usings
+
+using Microsoft.Practices.Prism.Logging;
+
+#endregion
+
+namespace LOB.Log.Interface
+{
+    public interface ILogger
+    {
+        void Log(string message, Category category, Priority priority);
+    }
+}
