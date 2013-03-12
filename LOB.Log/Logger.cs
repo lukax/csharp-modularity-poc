386e55c4aed6725c0df618f344b11f96b4acae35
Lucas <tecinfolucas@gmail.com>
2013-03-12 18:55:34 -0300

    added LOB.Log project to solution

---
+++ LOB.Log/Logger.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Practices.Prism.Logging;
+using log4net;
+using log4net.Config;
+
+namespace LOB.Log
+{
+    /// <summary>
+    /// A log4Net implementation of PRISM' ILoggerFacade.
+    /// *Note: Any logging framework could be plugged in here as long as we implement the ILoggerFacade interface.
+    /// </summary>
+    public class Logger : ILoggerFacade
+    {
+        // Member variables
+        private static readonly ILog _logger = LogManager.GetLogger(typeof(Logger));
+
+        public Logger()
+        {
+            XmlConfigurator.Configure();
+        }
+
+        #region ILoggerFacade Members
+
+        /// <summary>
+        /// Prism Log routine.
+        /// </summary>
+        /// <param name="message">The message to write.</param>
+        /// <param name="category">The message category.</param>
+        /// <param name="priority">Not used by Log4Net; pass Priority.None.</param>
+        public void Log(string message, Category category, Priority priority)
+        {
+            switch (category)
+            {
+                case Category.Debug:
+                    _logger.Debug(message);
+                    break;
+
+                case Category.Warn:
+                    _logger.Warn(message);
+                    break;
+
+                case Category.Exception:
+                    _logger.Error(message);
+                    break;
+
+                case Category.Info:
+                    _logger.Info(message);
+                    break;
+            }
+        }
+
+        #endregion ILoggerFacade Members
+    }
+}
---
+++ LOB.Log/Module.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Practices.Prism.Modularity;
+
+namespace LOB.Log
+{
+    public class Module:IModule
+    {
+        public void Initialize()
+        {
+            
+        }
+    }
+}
