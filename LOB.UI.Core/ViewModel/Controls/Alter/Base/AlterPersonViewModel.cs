4839314dd9f9d1d6d883750142985a46c0c2dbd5
Lukax <lucas.espindola@outlook.com>
2013-04-11 17:04:04 -0300

    minor fix in alterperson couldnt resolve component

--- LOB.UI.Core/ViewModel/Controls/Alter/Base/AlterPersonViewModel.cs
+++ LOB.UI.Core/ViewModel/Controls/Alter/Base/AlterPersonViewModel.cs
@@ -1,5 +1,6 @@
 ﻿#region Usings
 
+using System;
 using System.Collections.Generic;
 using LOB.Business.Interface.Logic.Base;
 using LOB.Dao.Interface;
@@ -18,36 +19,31 @@ using Microsoft.Practices.Unity;
 #endregion
 
 namespace LOB.UI.Core.ViewModel.Controls.Alter.Base {
-    public class AlterPersonViewModel : AlterBaseEntityViewModel<Person>, IAlterPersonViewModel {
+    public class AlterPersonViewModel : AlterBaseEntityViewModel<LocalPerson>, IAlterPersonViewModel {
 
         private readonly IPersonFacade _personFacade;
         private AlterAddressViewModel _alterAddressViewModel;
         private AlterContactInfoViewModel _alterContactInfoViewModel;
 
         [InjectionConstructor]
-        public AlterPersonViewModel(Person entity, IPersonFacade personFacade, AlterAddressViewModel alterAddressViewModel,
-            AlterContactInfoViewModel alterContactInfoViewModel, IRepository repository, IEventAggregator eventAggregator, ILoggerFacade logger)
+        public AlterPersonViewModel(IPersonFacade personFacade, AlterAddressViewModel alterAddressViewModel,
+            AlterContactInfoViewModel alterContactInfoViewModel, IRepository repository, IEventAggregator eventAggregator, ILoggerFacade logger,
+            LocalPerson entity = null)
             : base(entity, repository, eventAggregator, logger) {
             _personFacade = personFacade;
             _alterAddressViewModel = alterAddressViewModel;
             _alterContactInfoViewModel = alterContactInfoViewModel;
 
-            //TODO: Use business logic to set default params
-            if(Entity.Address.State == null && Entity.Address.Country == null) {
-                Entity.Address.Country = "Brasil";
-                Entity.Address.State = UFDictionary.Ufs[UF.RJ];
-            }
+            ////TODO: Use business logic to set default params
+            //if(entity != null)
+            //    if(Entity.Address.State == null && Entity.Address.Country == null) {
+            //        Entity.Address.Country = "Brasil";
+            //        Entity.Address.State = UFDictionary.Ufs[UF.RJ];
+            //    }
         }
 
         public IAlterAddressViewModel AlterAddressViewModel { get { return _alterAddressViewModel; } set { _alterAddressViewModel = value as AlterAddressViewModel; } }
         public IAlterContactInfoViewModel AlterContactInfoViewModel { get { return _alterContactInfoViewModel; } set { _alterContactInfoViewModel = value as AlterContactInfoViewModel; } }
-
-        public override void InitializeServices() {
-            Operation = _operation;
-            ClearEntity(null);
-        }
-
-        public override void Refresh() { ClearEntity(null); }
 
         protected override void Cancel(object arg) { EventAggregator.GetEvent<CloseViewEvent>().Publish(Operation); }
 
@@ -55,18 +51,18 @@ namespace LOB.UI.Core.ViewModel.Controls.Alter.Base {
             IEnumerable<ValidationResult> results;
             return _personFacade.CanAdd(out results);
         }
-
-        protected override bool CanCancel(object arg) { return true; }
-
-        protected override void ClearEntity(object arg) {
-            _alterAddressViewModel.ClearEntityCommand.Execute(null);
-            _alterContactInfoViewModel.ClearEntityCommand.Execute(null);
-            Entity = _personFacade.GenerateEntity();
-            _personFacade.SetEntity(Entity);
-            _personFacade.ConfigureValidations();
-        }
+        protected override void ClearEntity(object arg) { throw new NotImplementedException(); }
 
         private readonly UIOperation _operation = new UIOperation {Type = UIOperationType.Person, State = UIOperationState.Add};
+        #region Overrides of BaseViewModel
+
+        public override void InitializeServices() { }
+        public override void Refresh() { }
+
+        #endregion
+    }
+
+    public class LocalPerson : Person {
 
     }
 }
